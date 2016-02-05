66a2d6375c667e557f7a4472a9b8a8af09a7761b
kislayabhi <abhijeetkislay@gmail.com>
2016-02-04 22:56:29 -0600

    added the first odd even if else

--- fork_fun.c
+++ fork_fun.c
@@ -1,13 +1,39 @@
+/*
+
+What the program does:
+
+   This program is called forkfun.c. When M is started, it checks its own process
+id number. If the process id number is even, then M creates two child processes
+(called a1 and b1). If the process id number is odd, then M creates three child
+processes (called a1, b1, and c1). In either case, the child processes created
+should behave as follows: if the process id number of the child process is even,
+it should exec the fork2.c program, else it should exec the fork3.c program.
+Process M should do some form of wait to see which of its child proceses
+terminates first, report a message to this effect, and so on, until it finds
+that its last child process has terminated, at which point it reports a message
+to this effect. Process M then says "Goodbye!" and exits.
+
+How to use it
+
+    1.compile gcc -o forkfun forkfun.c
+    2.type forkfun following the command prompt
+*/
+
 #include<stdio.h>
 int main()
 {
-  // pid stores the process id of the child
-  int pid=fork();
+  int current_pid = getpid();
 
-  if(pid>0)
-    printf("Parent Process PID: %d  \n", getpid());
-  else if(pid==0)
-    printf("Child Process PID: %d \n", getpid());
+  // If the current process id is even, we create 2 child processes.
+  if(current_pid%2==0)
+  {
+    int a1 = fork();
+    int b1 = fork();
+  }
   else
-    printf("fork() couldn't be done");
+  {
+    int a1 = fork();
+    int b1 = fork();
+    int c1 = fork();
+  }
 }
