bc1d80a906fdacc27f7677772412db5a4110c2d1
kislayabhi <abhijeetkislay@gmail.com>
2016-02-07 15:18:02 -0600

    my final corrected version

--- CountThings/count.c
+++ CountThings/count.c
@@ -1,35 +1,32 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-
-int digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-bool is_digit(int character) {
-	for(int i=0; i<10; ++i)
-		if(character == digits[i])
-			return true;
-
-	return false;
-}
-
 /* Count digits, white spaces characters (blank, tab, newline),
    and of all other characters. */
 
 int main()
 {
 	int c, ndigit, nwhite, nother;	
-	ndigit = nwhite = nother = 0;
+	int digitcount[10];
 	
+	ndigit = nwhite = nother = 0;
+	for(int i=0; i<10; ++i)
+		digitcount[i] = 0;
+			
+
 	while((c = getchar()) != EOF) {
-		if(is_digit(c))
-			++ndigit;
+		if(c >= '0' && c <= '9')
+			++digitcount[c-'0'];
 		else if(c == ' ' || c == '\t' || c == '\n')
 			++nwhite;
 		else
 			++nother;
 	}
 
-	printf("\nno. of digits:\t%d\n", ndigit);
-	printf("no. of whitespaces:\t%d\n", nwhite);
-	printf("no. of other:\t%d\n", nother);
+	printf("\nno. of digits:\n");
+	for(int i=0; i<10; ++i)
+		printf("\t%2d count:\t%2d \n", i, digitcount[i]);
+
+	printf("no. of whitespaces:\t%2d\n", nwhite);
+	printf("no. of other:\t%2d\n", nother);
 }
