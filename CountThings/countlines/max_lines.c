8a6a147e4c7a988ba5f328493f4934eebd6444dd
kislayabhi <abhijeetkislay@gmail.com>
2016-02-09 07:48:33 -0600

    final implementation

--- CountThings/countlines/max_lines.c
+++ CountThings/countlines/max_lines.c
@@ -6,52 +6,43 @@
 int get_line(FILE *fp, char curr_array[]);
 void save(char src[], char dest[], int length);
 
+/* Print longest input line */
 int main()
 {
-	FILE *fp = fopen("max_lines.c", "r");
+	int maxline_len, currline_len;
 	char curr_line[MAXLINELEN]; /* Stores the current line */
 	char max_line[MAXLINELEN];  /* Stores the longest line */
-	int maxline_length, currline_length;
+	FILE *fp = fopen("max_lines.c", "r");
 
-	maxline_length = currline_length = 0;
-
-	while((currline_length = get_line(fp, curr_line)) >= 0 )
-		if(currline_length > maxline_length) {
-			maxline_length = currline_length;
-			save(curr_line, max_line, maxline_length);
+	maxline_len = currline_len = 0;
+	while ((currline_len = get_line(fp, curr_line)) > 0)
+		if (currline_len > maxline_len) {
+			maxline_len = currline_len;
+			save(curr_line, max_line, maxline_len);
 		}
-	
-	for(int i=0; i<maxline_length; ++i)
-		printf("%c", max_line[i]);
+	if (maxline_len > 0) /* there was a line above */
+		printf("%s", max_line);
 	printf("\n");
-	
 	free(fp);
+	return 0;
 }
 
-
+/* copy: copy the array from source to destination */
 void save(char src[], char dest[], int length) {
 	for(int i = 0; i < length; ++i)
-		dest[i]=src[i];
+		dest[i] = src[i];
 }
 
-
+/* get_line: read a line into curr_array, return length */
 int get_line(FILE *fp, char curr_array[]) {
 	char c;
-	int line_length = 0;
-	
-	if((c = getc(fp)) == EOF)
-		return -1;
-	else {
-		do {	
-			if(c != '\n' && c != EOF) {
-				curr_array[line_length] = c;
-				++line_length;
-			}
-			else {
-				break;
-			}
-		} while((c = getc(fp)) != EOF);
-	}
-	
-	return line_length;
+	int line_len;
+
+	for(line_len=0; ((c=getc(fp))!='\n') && (c!=EOF); ++line_len)
+		curr_array[line_len] = c;
+	if(c == '\n') {
+		curr_array[line_len] = c;
+		++line_len;
+	}	
+	return line_len;
 }
